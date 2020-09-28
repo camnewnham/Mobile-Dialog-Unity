4cca43c611ae0a6d1d961254b6ea2b08e3d644f3
Cam Newnham <cam@fologram.com>
2020-09-28 10:08:02 +1000

    Add editor actions for dialogs

--- native-dialog/Assets/MobileNative/Scripts/Dialog/NativeDialog.cs
+++ native-dialog/Assets/MobileNative/Scripts/Dialog/NativeDialog.cs
@@ -10,15 +10,49 @@ namespace pingak9
 
         public static void OpenDialog(string title, string message, string ok = "Ok", Action okAction = null)
         {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorUtility.DisplayDialog(title, message, ok))
+            {
+                okAction?.Invoke();
+            }
+#else
             MobileDialogInfo.Create(title, message, ok, okAction);
+#endif
         }
         public static void OpenDialog(string title, string message, string yes, string no, Action yesAction = null, Action noAction = null)
         {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorUtility.DisplayDialog(title, message, yes,no))
+            {
+                yesAction?.Invoke();
+            }
+            else
+            {
+                noAction?.Invoke();
+            } 
+#else
             MobileDialogConfirm.Create(title, message, yes, no, yesAction, noAction);
+#endif
         }
         public static void OpenDialog(string title, string message, string accept, string neutral, string decline, Action acceptAction = null, Action neutralAction = null, Action declineAction = null)
         {
+#if UNITY_EDITOR
+            int res = UnityEditor.EditorUtility.DisplayDialogComplex(title, message, accept, decline, neutral);
+            switch (res)
+            {
+                case 0:
+                    acceptAction?.Invoke();
+                    return;
+                case 1:
+                    declineAction?.Invoke();
+                    return;
+                case 2:
+                    neutralAction?.Invoke();
+                    return;
+            }
+#else
             MobileDialogNeutral.Create(title, message, accept, neutral, decline, acceptAction, neutralAction, declineAction);
+#endif
         }
         public static void OpenDatePicker(int year , int month, int day, Action<DateTime> onChange = null, Action<DateTime> onClose = null)
         {
